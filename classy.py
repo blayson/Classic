ceba7fbb1d08bce7114ca457113ac852760eb1be
Paplauski Andrei <poplavski.andrei@gmail.com>
2017-06-24 02:34:23 +0200

    deleted LOL file; add comments;add functionality of additing people to trip

--- classy.py
+++ classy.py
@@ -2,16 +2,15 @@ class User:
     def __init__(self,name="Lecha",surname='Ermak',age=20,foto_path="/home/andrei/Загрузки/1.jpg",
                  kontakt_ref="https://www.facebook.com/profile.php?id=100010513029228&fref=hovercard&hc_location=chat"):
         # self.id = id
-        self.name = name
-        self.surname = surname
-        self.age = age
-        self.foto_path = foto_path
-        self.kontakt_path = kontakt_ref
-
+        self.name = name        #имя пользователя
+        self.surname = surname  #фамилия пользователя
+        self.age = age          #возраст пользователя
+        self.foto_path = foto_path          #путь к фото
+        self.kontakt_path = kontakt_ref     #ссылка на профиль(FB, VK ...)
 
     def create_trip(self,dest,date,time,time_diff=3600,max_cap = 20):
         trip = Trip(dest,dest,date,time,time_diff,max_cap)
-        trip.partic.append(self)
+        trip.add_partic(self)
         return trip
 
 
@@ -19,21 +18,25 @@ class User:
 class Trip:
     def __init__(self, dest,date,time,time_diff=3600,max_cap = 20):
         # self.id = id
-        self.dest = dest
-        self.date = date
-        self.time = time
-        self.time_diff = time_diff
-        self.max_cap = max_cap
-        self.partic = []
+        self.dest = dest    #аэропорт
+        self.date = date    #дата приезда на место встречи
+        self.time = time    #время приезда на место встречи
+        self.time_diff = time_diff   #время в секундах которое эта группа может подождать
+        self.max_cap = max_cap       #максимальное количество людей в путешествии
+        self.partic = []             #список всех участников
+
+    def add_partic(self,user):
+        if(not isinstance(user,User) ):
+            raise Exception("Can't add this user")
+        self.partic.append(user)
 
 
 class Flight:
     def __init__(self,from_tmsp,to_tmsp,cost,duration,from_location):
         # self.id = id
-        self.from_tmsp = from_tmsp
-        self.to_tmsp = to_tmsp
-        self.cost = cost
-        self.duration = duration
+        self.from_tmsp = from_tmsp  #временной штамп отлёта
+        self.to_tmsp = to_tmsp      #временной штамп прилёта
+        self.cost = cost            #цена перелёта
+        self.duration = duration    #длительность перелёта
         #TODO inicialization with sygic
-        self.from_location = from_location
-
+        self.from_location = from_location  #кортеж (город, аэропорт)
