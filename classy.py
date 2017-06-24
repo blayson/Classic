ab0552ece39bd55cba9e69bc673ea62986612e23
Paplauski Andrei <poplavski.andrei@gmail.com>
2017-06-24 12:42:24 +0200

    add find_flights finall version

--- classy.py
+++ classy.py
@@ -1,3 +1,6 @@
+import requests
+import arrow
+
 class User:
     def __init__(self,name="Lecha",surname='Ermak',age=20,foto_path="/home/andrei/Загрузки/1.jpg",
                  kontakt_ref="https://www.facebook.com/profile.php?id=100010513029228&fref=hovercard&hc_location=chat"):
@@ -36,6 +39,44 @@ class Trip:
             raise Exception("Can't add this user")
         #TODO убрать из базы данных
         self.partic.remove(user)
+
+    def find_flights(self, fly_to, date_to, fly_from, time_to=None):
+        api_url = 'https://api.skypicker.com/flights'
+
+        adt = arrow.get(date_to, 'DD/MM/YYYY')
+        date_from = adt.shift(days=-1).format('DD/MM/YYYY')
+        print(date_from, date_to)
+
+        params = {
+            'flyFrom': fly_from,
+            'to': fly_to,
+            'dateFrom': date_from,
+            'dateTo': date_to,
+            'partner': 'picky',
+            'typeFlight': 'oneway',
+        }
+        response = requests.get(api_url, params=params).json()
+
+        data = []
+        time_diff = self.time_diff * 2
+        for flight in response['data']:
+            if time_to:
+                user_dt = arrow.get('{} {}'.format(date_to, time_to), 'DD/MM/YYYY HH:mm')
+                flight_dt_max = user_dt.timestamp + time_diff
+                flight_dt_min = user_dt.timestamp - time_diff
+                if (int(flight['aTimeUTC']) > flight_dt_min) and (int(flight['aTimeUTC']) < flight_dt_max):
+                    fo = Flight(from_tmsp=flight['dTimeUTC'], to_tmsp=flight['aTimeUTC'], cost=flight['price'],
+                                duration=flight['fly_duration'], from_location=(flight['cityFrom'], flight['flyFrom']),
+                                book_url=flight['deep_link'])
+                    data.append(fo)
+            else:
+                if arrow.get(flight['aTimeUTC']).format('DD/MM/YYYY') == arrow.get(date_to, 'DD/MM/YYYY').format(
+                        'DD/MM/YYYY'):
+                    fo = Flight(from_tmsp=flight['dTimeUTC'], to_tmsp=flight['aTimeUTC'], cost=flight['price'],
+                                duration=flight['fly_duration'], from_location=(flight['cityFrom'], flight['flyFrom']),
+                                book_url=flight['deep_link'])
+                    data.append(fo)
+        return data
 
 
 class Flight:
